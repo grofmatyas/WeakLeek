4f74b73750326a73db2532aa4c99afcf1d456836
matyas.grof <matyas.grof@cdn77.com>
2023-06-02 17:44:00 +0200

    feat: dashboard styling

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,10 +3,6 @@ import './Dashboard.css';
 import LineChart from '../components/LineChart';
 import PieChart from '../components/PieChart';
 
-const headingStyles = {
-  textAlign: 'center',
-};
-
 const Tab1: React.FC = () => {
   return (
     <IonPage>
@@ -18,28 +14,36 @@ const Tab1: React.FC = () => {
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Dashboard</IonTitle>
+            <IonTitle style={{ textAlign: 'center' }} size="large">Dashboard</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <div>
-          <h3 style={headingStyles}>Timelapse of thrown items</h3>
+        <div style={{ maxWidth: '100%', overflowX: 'auto' }}>
+          <h3 style={{ textAlign: 'center' }}>Timelapse of thrown items</h3>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <div style={{ width: '80%' }}>
           <LineChart data={{
             labels: ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Today'],
             datasets: [{
-              label: 'vegetable',
+              label: 'Vegetable',
               data: [0,5,0,0,0,5,4,0,0],
             }, {
-              label: 'meat',
+              label: 'Meat',
               data: [0,0,0,1,0,0,1,0],
             }],
           }} />
+          </div>
+        </div>
         </div>
         <div>
-          <h3 style={headingStyles}>Ratio of thrown categories</h3>
-          <PieChart data={{
-            labels: ['vegetable', 'meat'],
-            values: [14,2],
-          }} />
+          <h3 style={{ textAlign: 'center' }}>Ratio of thrown categories</h3>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <div style={{ width: '80%' }}>
+            <PieChart data={{
+              labels: ['Vegetable', 'Meat'],
+              values: [14, 2],
+            }} />
+          </div>
+        </div>
         </div>
       </IonContent>
     </IonPage>
