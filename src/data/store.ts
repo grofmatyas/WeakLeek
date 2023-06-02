d888041d78dd220b926de6341d71653d56f19122
matyas.grof <matyas.grof@cdn77.com>
2023-06-02 18:21:28 +0200

    feat: add first version of store

---
+++ src/data/store.ts
@@ -0,0 +1,67 @@
+import { Storage, Drivers } from "@ionic/storage";
+
+let storage: Storage | false = false;
+
+export const createStore = async (name = "__mydb"): Promise<void> => {
+  storage = new Storage({
+    name,
+    driverOrder: [Drivers.IndexedDB, Drivers.LocalStorage]
+  });
+
+  await storage.create();
+};
+
+export const set = (key: string, val: string): void => {
+  if (storage) {
+    storage.set(key, val);
+  }
+};
+
+export const get = async (key: string): Promise<string | null> => {
+  if (storage) {
+    const val = await storage.get(key);
+    return val;
+  }
+  return null;
+};
+
+export const remove = async (key: string): Promise<void> => {
+  if (storage) {
+    await storage.remove(key);
+  }
+};
+
+export const clear = async (): Promise<void> => {
+  if (storage) {
+    await storage.clear();
+  }
+};
+
+export const setObject = async (key: string, id: string, val: Record<string, any>): Promise<void> => {
+  if (storage) {
+    const all = await storage.get(key);
+    const objIndex = all.findIndex((a: any) => parseInt(a.id) === parseInt(id));
+
+    all[objIndex] = val;
+    set(key, all);
+  }
+};
+
+export const removeObject = async (key: string, id: string): Promise<void> => {
+  if (storage) {
+    const all = await storage.get(key);
+    const objIndex = all.findIndex((a: any) => parseInt(a.id) === parseInt(id));
+
+    all.splice(objIndex, 1);
+    set(key, all);
+  }
+};
+
+export const getObject = async (key: string, id: string): Promise<Record<string, any> | null> => {
+  if (storage) {
+    const all = await storage.get(key);
+    const obj = all.filter((a: any) => parseInt(a.id) === parseInt(id))[0];
+    return obj;
+  }
+  return null;
+};
