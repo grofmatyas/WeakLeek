bd39cbf3641c8ffaa6d8989a3bd10fdb4a52c036
matyas.grof <matyas.grof@cdn77.com>
2023-06-02 22:11:48 +0200

    icon for doubka

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,6 +1,7 @@
 import { IonAlert, IonButton, IonIcon, IonItem, IonLabel } from "@ionic/react";
 import { FC } from "react";
-import { sad, trash } from "ionicons/icons";
+// import { sad, trash } from "ionicons/icons";
+import dontEat from '../dontEat.svg';
 import { set } from "../data/store";
 
 const Item: FC<{ name: string; category: string; setAlert: any }> = ({
@@ -12,7 +13,7 @@ const Item: FC<{ name: string; category: string; setAlert: any }> = ({
     <IonItem>
       <IonLabel>{name}</IonLabel>
       <IonButton slot="end" onClick={() => setAlert(true)}>
-        <IonIcon slot="icon-only" icon={sad} />
+        <IonIcon slot="icon-only" icon={dontEat} />
       </IonButton>
     </IonItem>
   );
